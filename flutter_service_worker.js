0353a9cd6598c26e6c8af390f525c45cb8953ec0
panzhoutao <836618033@qq.com>
2020-07-09 21:23:35 +0800

    Initial commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,7 +5,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "2f3b2feb8afdc8569611f287a0c11804",
 "/": "2f3b2feb8afdc8569611f287a0c11804",
-"main.dart.js": "2d03cf598c43205113b04157a6b64e56",
+"main.dart.js": "12e4d6c9998237689ec2dfccce47d9c0",
 "favicon.png": "88493d315c57f8db5a5eeeff146ac995",
 "icons/Icon-192.png": "3f052ce7c558ba21ffa531f18b865f11",
 "icons/Icon-512.png": "518babda2ce7b91dbb5cdc720bff78e0",
